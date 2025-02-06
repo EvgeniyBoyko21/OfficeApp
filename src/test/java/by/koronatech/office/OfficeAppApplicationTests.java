cdfabe4a47206a66baaced1feb18f87abc398b4c
Evgeniy Boiko <3kwerone@mail.ru>
2025-02-06 15:46:34 +0300

    Initial commit

---
+++ src/test/java/by/koronatech/office/OfficeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.koronatech.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
